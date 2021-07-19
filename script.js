7296b1c29249923f50b4a5ded089bec5c33c3d63
bensammwaniki <bensammwaniki@gmail.com>
2021-07-19 08:13:19 +0300

    manipulating data with javascript

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 // Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 
 // getting the data from the form 
-var dataOne = document.getElementById("name").value
-var dataTwo = document.getElementById("date").value
-var dataThree = document.getElementById("male").checked
-var dataFour = document.getElementById("female").checked
 
-// calculate day of the week 
-var date = dataTwo.replace(/-/g, "")
-var arr = Array.from(date)
-var cent = parseInt(arr[0]+arr[1])
-var year = parseInt(arr[2]+arr[3])
-var month = parseInt(arr[4]+arr[5])
-var day = parseInt(arr[6]+arr[7])
 
 function dayOfWeek(){
+
+            var dataOne = document.getElementById("name").value
+            var dataTwo = document.getElementById("date").value
+            var dataThree = document.getElementById("male").checked
+            var dataFour = document.getElementById("female").checked
+
+            // calculate day of the week 
+            var date = dataTwo.replace(/-/g, "")
+            var arr = Array.from(date)
+            var cent = parseInt(arr[0]+arr[1])
+            var year = parseInt(arr[2]+arr[3])
+            var month = parseInt(arr[4]+arr[5])
+            var day = parseInt(arr[6]+arr[7])
+
     var when = ( ( (cent/4) -2*cent-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day )%7
     var dayW = Math. trunc(when);
     var arryDays = [
         male = {
-        Saturday: "Kwame",
+        Saturday:"Kwame",
         Sunday: "Kwasi",
         Monday: "Kwadwo",
         Tuesday: "Kwabena",
@@ -37,16 +40,21 @@ function dayOfWeek(){
             Friday: "Afua"
                     }
                       ]
-if(dataThree == false && dataFour == false){
- alert("please input your gender")
-}
-if(dataTwo == ""){
-    alert("please input your DOB")
 
-}
+
+                            if(dataThree == false && dataFour == false){
+                            alert("please input your gender")
+                            }
+                            if(dataTwo === ""){
+                                alert("please input your DOB")
+
+                            }
+                            if(dataThree == true && dayW == 0){
+                                
+                                   console.log(arryDays[0].Saturday)
+                            }
 
 
 
 return dayW
-}
-
+} 
